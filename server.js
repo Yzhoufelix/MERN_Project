5e53283a2cadf6f7325247755c5db653a3b45973
Yzhoufelix <felix.zhouyf.ca@gmail.com>
2019-12-02 22:41:52 -0500

    test post && add validation

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 // Connect database
 connectDB();
 
+// Init Middleware
+app.use(express.json({ extended: false }));
+
 app.get("/", (req, res) => res.send("API Running"));
 
 //Define Routes
