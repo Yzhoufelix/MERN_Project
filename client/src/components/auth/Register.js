3dd2fe8bccd31e75aa41133f4e3095fbab5c9d07
Yzhoufelix <felix.zhouyf.ca@gmail.com>
2020-01-03 16:29:31 -0500

    use redux for alert part(reducers,actions)

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,8 +1,11 @@
 import React, { Fragment, useState } from "react";
+import { connect } from "react-redux";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import { setAlert } from "../../actions/alert";
+import PropTypes from "prop-types";
 
-const Register = () => {
+const Register = ({ setAlert }) => {
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -18,7 +21,7 @@ const Register = () => {
   const onSubmit = async e => {
     e.preventDefault();
     if (password !== password2) {
-      console.log("Passwords do not match");
+      setAlert("Passwords do not match", "danger", 3000);
     } else {
       console.log("SUCCESS");
       // Working Without Redux
@@ -104,4 +107,8 @@ const Register = () => {
   );
 };
 
-export default Register;
+Register.propTypes = {
+  setAlert: PropTypes.func.isRequired
+};
+
+export default connect(null, { setAlert })(Register);
---
+++ client/src/reducers/auth.js
@@ -0,0 +1,17 @@
+import { REGISTER_SUCCESS, REGISTER_FAIL } from "../actions/types";
+
+const initialState = {
+  token: localStorage.getItem("token"),
+  isAuthenticated: null,
+  loading: true,
+  user: null
+};
+
+export default function(state = initialState, action) {
+  const { type, payload } = action;
+
+  switch (type) {
+    case REGISTER_SUCCESS:
+      localStorage.setItem("token", payload.token);
+  }
+}
